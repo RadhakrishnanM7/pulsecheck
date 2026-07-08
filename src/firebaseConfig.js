d27d18aa39f05c5571a527b18fa5866c6ee3b1ac
RadhakrishnanM7 <rakrims@gmail.com>
2026-07-08 21:00:24 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -16,12 +16,6 @@ export const firebaseConfig = {
   appId: "1:1051167812921:web:1672fc9497f9670ef18ce8"
 };
 
-const isConfigured = !String(firebaseConfig.apiKey).startsWith(
-  apiKey: "AIzaSyABRNWoF2zoX0LjZrsjHZ52523df0H26ko",
-  authDomain: "pulsecheck-d7d4d.firebaseapp.com",
-  projectId: "pulsecheck-d7d4d",
-  storageBucket: "pulsecheck-d7d4d.firebasestorage.app",
-  messagingSenderId: "1051167812921",
-  appId: "1:1051167812921:web:1672fc9497f9670ef18ce8");
+const isConfigured = !String(firebaseConfig.apiKey).startsWith("AIza");
 export const app = isConfigured ? initializeApp(firebaseConfig) : null;
 export const db = app ? getFirestore(app) : null;
